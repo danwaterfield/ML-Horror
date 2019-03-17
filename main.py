de63224973a59714cc6f345d9ed03ce020671eb1
Dan Waterfield <36337888+danwaterfield@users.noreply.github.com>
2019-03-17 17:22:21 +0000

    script for getting round ssh failure for downloading nltk corpus sets

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #! Python3 -- applies ml to kaggle horror author dataset to identify authorship of unknown sentences. Useful for wider 18c project...
 
-
 import numpy as np 
 import pandas as pd 
-import nltk 
+import nltk
 import string
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,7 +12,7 @@ df = pd.read_csv('~/documents/github/ml-horror/train.csv')
 #print(df.head(5)) if you want to check it's inputted correctly
 
 # removes punctuation for word counting purposes.
-no_punct_translator=str.maketrans('','',string.punctuation)
+no_punct_translator = str.maketrans('','', string.punctuation)
 # tokenize each sentence and remove punctuation
 df['words'] = df['text'].apply(lambda t: nltk.word_tokenize(t.translate(no_punct_translator).lower()))
 
@@ -25,7 +24,7 @@ df['sentence_length'] = df['words'].apply(lambda w: sum(map(len, w)))
 # for future calculations, let's keep around the full text length, including punctuation
 df['text_length'] = df['text'].apply(lambda t: len(t))
 
-#print(df.head(5)) checks everything's working correctly: it is!
+print(df.head(5)) #checks everything's working correctly: it is!
 
 
 sns.set_style('whitegrid') # sets style for graphs imported above
@@ -37,4 +36,6 @@ sns_plot = sns.boxplot(x = "author", y = "word_count", data=df, color = "red") #
 fig = sns_plot.get_figure()
 fig.savefig('plot.png')
 """
-sns.boxplot(x = "author", y = "word_count", data=df, color = "red") # plots word per sentence.
+#sns.boxplot(x = "author", y = "word_count", data=df, color = "red") # plots word per sentence.
+
+#print(str(df.groupby(['author'])['sentence_length'].describe())
